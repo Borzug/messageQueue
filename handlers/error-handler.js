f4e975dfc1db12e198e52f00c851b9a12266f599
Denis <tendomail@mail.ru>
2017-09-03 11:18:22 +0300

    no message

--- handlers/error-handler.js
+++
@@ -1,5 +0,0 @@
-module.exports = function printError(errorQueue, cb) {
-    errorQueue.dequeue(function(message) {
-        console.log(message);
-        printError(errorQueue, cb);
-    }, cb)};
--- utils/utils.js
+++
@@ -1,8 +0,0 @@
-function guardIsFunction(value) {
-    if (!typeof value === 'function')
-        throw new Error('Value is null or not a function');
-}
-
-module.exports = {
-    guardIsFunction
-};
